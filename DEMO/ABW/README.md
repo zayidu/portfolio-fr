ad688c9f91b35fb7460c993889ed166990f5b906
Zayidu Ansari <zayidu11@gmail.com>
2024-01-02 07:05:05 +0100

    Bootsrapped portfolio-fr

---
+++ DEMO/ABW/README.md
@@ -0,0 +1,12 @@
+# Advanced Booking Widget:
+
+## Mobile - EN:
+![Advanced Booking Widget Mobile View - AR](./MOBILE/en_mobile.gif)
+## Mobile - AR:
+![Advanced Booking Widget Mobile View - EN](./MOBILE/ar_mobile.gif)
+
+## Desktop - EN:
+![Advanced Booking Widget Desktop View - EN](./DESKTOP/en_desktop.gif)
+
+## Desktop - AR:
+![Advanced Booking Widget Desktop View - AR](./DESKTOP/ar_desktop.gif)
---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+const onOpenInsta = () => {
+  // window.location.hostname === "zayidu.github.io" &&
+  window.open('https://instaclonezayidu.herokuapp.com/', '_blank').focus();
+};
+
+const onOpenzDevConnector = () => {
+  window.open('https://powerful-brook-38361.herokuapp.com/', '_blank').focus();
+};
+
+const onOpenNBA = () => {
+  window.open('https://nba-full-stack-59c4b.web.app/', '_blank').focus();
+};
+
+const onOpenBookaholic = () => {
+  window.open('https://fierce-brook-03104.herokuapp.com/', '_blank').focus();
+};
